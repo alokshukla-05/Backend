64f8b8c8cae504c9040c32ca03f10f14e2e98f68
ahaag4 <aalokshukla94@gmail.com>
2026-09-07 21:16:53 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv'
+import express from 'express';
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env'
+})
+
+const app = express();
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGO_DB connection failed!! ", error)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ( async () => {
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", (error) => {
+//         console.log("ERROR: ",error);
+//         throw error
+//        })
+
+//        app.listen(process.env.PORT, () => {
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//        })
+
+
+
+//     } catch (error) {
+//         console.log("ERROR: ",error);
+//         throw error
+//     }
+// })()
