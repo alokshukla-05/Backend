ebfafdf00a648cd0eef97b26df13169a89149530
ahaag4 <aalokshukla94@gmail.com>
2026-09-14 18:10:32 +0530

    controoler

--- src/app.js
+++ src/app.js
@@ -16,6 +16,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from './routes/user.routes.js';
+
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+//working http://localhost:8000/api/v1/users/register
 
 
 export { app }
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,22 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler( async (req, res) => {
+    // res.status(200).json({
+    //     message: "ok"
+    // })
+    // step:
+    // 1.get user details from frontend
+    // 2.validation - ot empty
+    // 3.check if user already exist: username,email
+    // 4.check for images, check for avatar
+    // 5.upload them to cloudinary, avatar
+    // 6.create user object - create entry in db
+    // 7.remove password and refresh tokken field from response
+    // 8.check for user creation
+    // 9.return res
+
+    const {fullName, email, username, password} = req.body
+    console.log("email:", email);
+})
+
+export { registerUser }
